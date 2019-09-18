899350d21fe7f13fcc4b0db5461c26c68bd6b8f7
Chris Brown <chris.brown@nwyc.com>
2019-09-18 14:07:03 -0500

    creating setup and fixing some bugs

---
+++ setup.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from distutils.core import setup
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'pynamematcher'))
+from version import __version__
+
+setup(
+    name='PyNameMatcher',
+    version=__version__,
+    author='Constituent Voice LLC',
+    author_email='chris.brown@constituentvoice.com',
+    packages=['pynamematcher'],
+    url='https://github.com/constituentvoice/PyNameMatcher',
+    license='Apache',
+    description='Simple library for matching first names with possible variations',
+    long_description=open('README.rst').read(),
+    install_requires=['Metaphone']
+)
