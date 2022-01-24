f0ab4851c58329d313dbf8209958e05064e89cee
Chris Brown <chris.brown@nwyc.com>
2022-01-24 09:55:07 -0600

    added tests. Added gitignore

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,31 +1,28 @@
-import unittest
+import pytest
 from pynamematcher import PyNameMatcher
 
 
-class TestPyNameMatcher(unittest.TestCase):
+class TestPyNameMatcher:
     def test_match_with_metaphone(self):
         pm = PyNameMatcher(use_metaphone=True)
 
         # "robart" won't currently match without metaphone.
-        self.assertGreater(len(pm.match('Robart')), 0)
+        assert len(pm.match('Robart')) > 0
+
+    def test_no_match_no_metaphone(self):
+        pm = PyNameMatcher()
+
+        assert len(pm.match('Robart')) < 1
 
     def test_match_no_metaphone(self):
-        pm = PyNameMatcher()
-        self.assertLess(len(pm.match('Robart')), 1)
-
         pm2 = PyNameMatcher()
-        self.assertGreater(len(pm2.match('Robert')), 1)
+        assert len(pm2.match('Robert')) > 0
 
     def test_nomatch(self):
         pm = PyNameMatcher()
-        self.assertIsInstance(pm.match('gobblygoop'), set)
+        assert isinstance(pm.match('gobblygoop'), set)
 
     def test_nomatch_return_none(self):
         pm = PyNameMatcher()
-        self.assertIs(pm.match('gobblygoop', empty_match_returns_none=True), None)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+        assert pm.match('gobblygoop', empty_match_returns_none=True) is None
 
